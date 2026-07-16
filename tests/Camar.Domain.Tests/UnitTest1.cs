df1fb781e37d7bad8177e7df4b2e58eaa89f0c81
MendietaGarciaAlejandro <153531705+MendietaGarciaAlejandro@users.noreply.github.com>
2026-07-16 21:43:07 +0200

    proyectos de test con xunit

---
+++ tests/Camar.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Camar.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/Camar.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Camar.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
